1b140b09216feb6efcb3ef4df02ab4acd4cdd478
Zeon O <obertm1415@my.uwstout.edu>
2024-10-09 11:54:00 -0500

    Adjusted Tile set and sorting
    Reimported tile set.
    Added base border layer with sprite collider.
    Adjusted sorting layer

--- Assets/Scripts/UsedMapBetter/GameCamera.cs
+++ Assets/Scripts/UsedMapBetter/GameCamera.cs
@@ -6,15 +6,10 @@ public class GameCamera : MonoBehaviour
 {
     [SerializeField] Material GLDraw;
     GameObject player;
-    float camHeight, camWidth;
-    Camera cam;
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-        cam = Camera.main;
-        camHeight = 2 * cam.orthographicSize;
-        camWidth = camHeight * cam.aspect;
     }
 
     void LateUpdate()
@@ -23,17 +18,5 @@ public class GameCamera : MonoBehaviour
         pos.x = player.transform.position.x;
         pos.y = player.transform.position.y;
         transform.position = pos;
-    }
-
-    private void OnPostRender()
-    {
-        float cameraLeft = transform.position.x - camWidth / 2;
-        float cameraBottom = transform.position.y - camHeight / 2;
-
-        GL.PushMatrix();
-        GLDraw.SetPass(0);
-        GL.LoadOrtho();
-
-        GL.PopMatrix();
     }
 }
