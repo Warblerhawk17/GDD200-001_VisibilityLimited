d7d628f3a4404b8a5ca0a9b3f9acb86aab44413b
OnyxSeverin1012 <severino1012@my.uwstout.edu>
2024-11-01 11:37:14 -0500

    Revert "Reverted some changes"
    This reverts commit 24901a04f2528c6253ed9050d1d261206d9258c1.

--- Assets/Scripts/Lights/GroundLightBehavior.cs
+++ Assets/Scripts/Lights/GroundLightBehavior.cs
@@ -1,7 +1,3 @@
-// Amelia Nehring
-// GDD 200-001
-// Fall 2024
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -15,14 +11,15 @@ public class GroundLightBehavior : MonoBehaviour
     public TextMeshProUGUI pickupText;
     LightSpawner gameManager;
     [SerializeField] bool isNearLight = false;
-    string lightType;
-    string lightTypeRequested;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player");
         gameManager = GameObject.Find("Game Manager").GetComponent<LightSpawner>();
         pickupText = GameObject.Find("Pickup Text").GetComponentInChildren<TextMeshProUGUI>();
+        pickupText.SetText("Press E to pickup" + " Flashlight");
+
+
     }
 
     // Update is called once per frame
@@ -36,36 +33,20 @@ public class GroundLightBehavior : MonoBehaviour
 
     void PromptUser()
     {
-
         distance = Vector2.Distance(player.transform.position, this.gameObject.transform.position);
         if (distance <= maxDistance)
         {
-            if(this.tag == "Ground Flashlight")
-            {
-                lightType = "Flashlight";
-            }
-            else if (this.tag ==  "Ground Candle")
-            {
-                lightType = "Candle";
-
-            }
-            else if (this.tag == "Ground Fireflies")
-            {
-                lightType = "Fireflies";
-
-            }
-            pickupText.SetText("Press E to pickup " + lightType);
             if (distance <= maxDistance && isNearLight == false) {
                 isNearLight = true;
                 pickupText.enabled = true;
 
             }
-            if (Input.GetKeyDown(KeyCode.E) && ((GameObject.FindWithTag("Flashlight") == false && GameObject.FindWithTag("Candle") == false && GameObject.FindWithTag("Fireflies") == false)))
+            if (Input.GetKeyDown(KeyCode.E) && (GameObject.FindWithTag("Flashlight") == false))
                 
             {
                 Destroy(this.gameObject);
                 gameManager.lightSpawnRequested = true;
-                gameManager.lightToSpawn = lightType;
+
                 pickupText.enabled = false;
             }
 
