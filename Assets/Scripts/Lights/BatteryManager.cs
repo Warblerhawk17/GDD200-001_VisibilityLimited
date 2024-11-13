a46e97c7df860ebe396abafe7bd1a55b6b70d421
amelia <nehringa6342@my.uwstout.edu>
2024-11-13 10:59:17 -0600

    moved ui to canvas

--- Assets/Scripts/Lights/BatteryManager.cs
+++ Assets/Scripts/Lights/BatteryManager.cs
@@ -2,16 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class BatteryManager : MonoBehaviour
 {
-    public SpriteRenderer spriteRenderer;
+   // public SpriteRenderer spriteRenderer;
     public Sprite[] flashlightBatterySprites;
     public Sprite[] candleBatterySprites;
     public Sprite[] firefliesBatterySprites;
     public List<Sprite> batterySprites;
     public float batteryCharge;
     [SerializeField] int lightNum = 0;
+    [SerializeField] UnityEngine.UI.Image batteryImage;
 
     // Timer and interval to decrease charge every second
     public float interval = 1f;
@@ -19,16 +22,15 @@ public class BatteryManager : MonoBehaviour
 
     void Start()
     {
-        spriteRenderer.enabled = false;
-
+        batteryImage.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (batteryCharge <= 0 && spriteRenderer.enabled)
+        if (batteryCharge <= 0)
         {
-            spriteRenderer.enabled = false;
+            batteryImage.enabled = false;
 
         }
         SetBatteryType();
@@ -43,23 +45,23 @@ public class BatteryManager : MonoBehaviour
             switch (batteryCharge)
             {
                 case >= 40f:
-                    spriteRenderer.enabled = true;
-                    spriteRenderer.sprite = batterySprites[0];
+                    batteryImage.enabled = true;
+                    batteryImage.sprite = batterySprites[0];
                     break;
                 case >= 30f:
-                    spriteRenderer.sprite = batterySprites[1];
+                    batteryImage.sprite = batterySprites[1];
                     break;
                 case >= 20f:
-                    spriteRenderer.sprite = batterySprites[2];
+                    batteryImage.sprite = batterySprites[2];
                     break;
                 case >= 10f:
-                    spriteRenderer.sprite = batterySprites[3];
+                    batteryImage.sprite = batterySprites[3];
                     break;
                 case >= 0f:
-                    spriteRenderer.sprite = batterySprites[4];
+                    batteryImage.sprite = batterySprites[4];
                     break;
                 case <= 0f:
-                    spriteRenderer.enabled = false;
+                    batteryImage.enabled = false;
                     break;
                 default:
                     break;
@@ -70,54 +72,54 @@ public class BatteryManager : MonoBehaviour
             switch (batteryCharge)
             {
                 case >= 140f:
-                    spriteRenderer.enabled = true;
-                    spriteRenderer.sprite = batterySprites[0];
+                    batteryImage.enabled = true;
+                    batteryImage.sprite = batterySprites[0];
                     break;
                 case >= 130f:
-                    spriteRenderer.sprite = batterySprites[1];
+                    batteryImage.sprite = batterySprites[1];
                     break;
                 case >= 120f:
-                    spriteRenderer.sprite = batterySprites[2];
+                    batteryImage.sprite = batterySprites[2];
                     break;
                 case >= 110f:
-                    spriteRenderer.sprite = batterySprites[3];
+                    batteryImage.sprite = batterySprites[3];
                     break;
                 case >= 100f:
-                    spriteRenderer.sprite = batterySprites[4];
+                    batteryImage.sprite = batterySprites[4];
                     break;
                 case >= 90f:
-                    spriteRenderer.sprite = batterySprites[5];
+                    batteryImage.sprite = batterySprites[5];
                     break;
 
                 case >= 80f:
-                    spriteRenderer.sprite = batterySprites[6];
+                    batteryImage.sprite = batterySprites[6];
                     break;
                 case >= 70f:
-                    spriteRenderer.sprite = batterySprites[7];
+                    batteryImage.sprite = batterySprites[7];
                     break;
                 case >= 60f:
-                    spriteRenderer.sprite = batterySprites[8];
+                    batteryImage.sprite = batterySprites[8];
                     break;
                 case >= 50f:
-                    spriteRenderer.sprite = batterySprites[9];
+                    batteryImage.sprite = batterySprites[9];
                     break;
                 case >= 40f:
-                    spriteRenderer.sprite = batterySprites[10];
+                    batteryImage.sprite = batterySprites[10];
                     break;
                 case >= 30f:
-                    spriteRenderer.sprite = batterySprites[11];
+                    batteryImage.sprite = batterySprites[11];
                     break;
                 case >= 20f:
-                    spriteRenderer.sprite = batterySprites[12];
+                    batteryImage.sprite = batterySprites[12];
                     break;
                 case >= 10f:
-                    spriteRenderer.sprite = batterySprites[13];
+                    batteryImage.sprite = batterySprites[13];
                     break;
                 case > 0f:
-                    spriteRenderer.sprite = batterySprites[14];
+                    batteryImage.sprite = batterySprites[14];
                     break;
                 case <= 0f:
-                    spriteRenderer.enabled = false;
+                    batteryImage.enabled = false;
                     break;
                 default:
                     break;
@@ -128,39 +130,39 @@ public class BatteryManager : MonoBehaviour
             switch (batteryCharge)
             {
                 case >= 90f:
-                    spriteRenderer.enabled = true;
-                    spriteRenderer.sprite = batterySprites[0];
+                    batteryImage.enabled = true;
+                    batteryImage.sprite = batterySprites[0];
                     break;
 
                 case >= 80f:
-                    spriteRenderer.sprite = batterySprites[1];
+                    batteryImage.sprite = batterySprites[1];
                     break;
                 case >= 70f:
-                    spriteRenderer.sprite = batterySprites[2];
+                    batteryImage.sprite = batterySprites[2];
                     break;
                 case >= 60f:
-                    spriteRenderer.sprite = batterySprites[3];
+                    batteryImage.sprite = batterySprites[3];
                     break;
                 case >= 50f:
-                    spriteRenderer.sprite = batterySprites[4];
+                    batteryImage.sprite = batterySprites[4];
                     break;
                 case >= 40f:
-                    spriteRenderer.sprite = batterySprites[5];
+                    batteryImage.sprite = batterySprites[5];
                     break;
                 case >= 30f:
-                    spriteRenderer.sprite = batterySprites[6];
+                    batteryImage.sprite = batterySprites[6];
                     break;
                 case >= 20f:
-                    spriteRenderer.sprite = batterySprites[7];
+                    batteryImage.sprite = batterySprites[7];
                     break;
                 case >= 10f:
-                    spriteRenderer.sprite = batterySprites[8];
+                    batteryImage.sprite = batterySprites[8];
                     break;
                 case > 0f:
-                    spriteRenderer.sprite = batterySprites[9];
+                    batteryImage.sprite = batterySprites[9];
                     break;
                 case <= 0f:
-                    spriteRenderer.enabled = false;
+                    batteryImage.enabled = false;
                     break;
                 default:
                     break;
